40c4469b3fc3121861871cd277eaa424089b48ae
wonder-mice <wonder.mice@gmail.com>
2014-12-16 16:53:50 -0800

    Add entry and head for list and stailq

--- zf_queue/zf_queue.hpp
+++ zf_queue/zf_queue.hpp
@@ -3,6 +3,77 @@
 #ifndef _ZF_QUEUE_H_
 #define _ZF_QUEUE_H_
 
+/*
+ * This file defines four types of data structures: singly-linked lists,
+ * singly-linked tail queues, lists and tail queues.
+ *
+ * A singly-linked list is headed by a single forward pointer. The elements
+ * are singly linked for minimum space and pointer manipulation overhead at
+ * the expense of O(n) removal for arbitrary elements. New elements can be
+ * added to the list after an existing element or at the head of the list.
+ * Elements being removed from the head of the list should use the explicit
+ * macro for this purpose for optimum efficiency. A singly-linked list may
+ * only be traversed in the forward direction.  Singly-linked lists are ideal
+ * for applications with large datasets and few or no removals or for
+ * implementing a LIFO queue.
+ *
+ * A singly-linked tail queue is headed by a pair of pointers, one to the
+ * head of the list and the other to the tail of the list. The elements are
+ * singly linked for minimum space and pointer manipulation overhead at the
+ * expense of O(n) removal for arbitrary elements. New elements can be added
+ * to the list after an existing element, at the head of the list, or at the
+ * end of the list. Elements being removed from the head of the tail queue
+ * should use the explicit macro for this purpose for optimum efficiency.
+ * A singly-linked tail queue may only be traversed in the forward direction.
+ * Singly-linked tail queues are ideal for applications with large datasets
+ * and few or no removals or for implementing a FIFO queue.
+ *
+ * A list is headed by a single forward pointer (or an array of forward
+ * pointers for a hash table header). The elements are doubly linked
+ * so that an arbitrary element can be removed without a need to
+ * traverse the list. New elements can be added to the list before
+ * or after an existing element or at the head of the list. A list
+ * may be traversed in either direction.
+ *
+ * A tail queue is headed by a pair of pointers, one to the head of the
+ * list and the other to the tail of the list. The elements are doubly
+ * linked so that an arbitrary element can be removed without a need to
+ * traverse the list. New elements can be added to the list before or
+ * after an existing element, at the head of the list, or at the end of
+ * the list. A tail queue may be traversed in either direction.
+ *
+ *                              SLIST	LIST	STAILQ	TAILQ
+ * _entry                       +       +       +       +
+ * _head                        +       +       +       +
+ * _initializer                 +       -       -       +
+ * _init                        +       -       -       +
+ * _empty                       +       -       -       +
+ * _FIRST                       +       -       -       +
+ * _NEXT                        +       -       -       +
+ * _PREV			-	+	-	+
+ * _LAST			-	-	+	+
+ * _FOREACH			+	+	+	+
+ * _FOREACH_FROM		+	+	+	+
+ * _FOREACH_SAFE		+	+	+	+
+ * _FOREACH_FROM_SAFE		+	+	+	+
+ * _FOREACH_REVERSE		-	-	-	+
+ * _FOREACH_REVERSE_FROM	-	-	-	+
+ * _FOREACH_REVERSE_SAFE	-	-	-	+
+ * _FOREACH_REVERSE_FROM_SAFE	-	-	-	+
+ * _INSERT_HEAD			+	+	+	+
+ * _INSERT_BEFORE		-	+	-	+
+ * _INSERT_AFTER		+	+	+	+
+ * _INSERT_TAIL			-	-	+	+
+ * _CONCAT			-	-	+	+
+ * _REMOVE_AFTER		+	-	+	-
+ * _REMOVE_HEAD			+	-	+	-
+ * _REMOVE			+	+	+	+
+ * _SWAP			+	+	+	+
+ */
+
+/*
+ * Singly-linked list
+ */
 template <typename T>
 struct zf_slist_entry
 {
@@ -73,6 +144,41 @@ void zf_slist_insert_after(zf_slist_head<T, entry> *const, T *const b, T *const 
 	zf_slist_insert_after(b, e, entry);
 }
 
+/*
+ * List
+ */
+template <typename T>
+struct zf_list_entry
+{
+	T *next;
+	zf_list_entry<T> *prev;
+};
+
+template <typename T, zf_list_entry<T> T:: *entry>
+struct zf_list_head
+{
+	T *first;
+};
+
+/*
+ * Singly-linked tail queue
+ */
+template <typename T>
+struct zf_stailq_entry
+{
+	T *next;
+};
+
+template <typename T, zf_list_entry<T> T:: *entry>
+struct zf_stailq_head
+{
+	T *first;
+	zf_stailq_entry<T> *last;
+};
+
+/*
+ * Tail queue
+ */
 template <typename T>
 struct zf_tailq_entry
 {
