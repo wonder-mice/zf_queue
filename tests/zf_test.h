edd2a62955a009d1b00ed5bc6457d9fe7596e8a7
wonder-mice <wonder.mice@gmail.com>
2015-04-26 17:13:16 -0700

    Fix C tests (exit with non-0 on failure)

--- tests/zf_test.h
+++ tests/zf_test.h
@@ -1,49 +1,45 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #if !defined(_ZF_TEST_STRINGIFY) && !defined(_ZF_TEST__STRINGIFY)
 #define _ZF_TEST__STRINGIFY(x) #x
 #define _ZF_TEST_STRINGIFY(x) _ZF_TEST__STRINGIFY(x)
 #endif
 
-typedef struct _zf_test_runner
-{
-    unsigned nerror;
-}
-_zf_test_runner;
-
 #define TEST_RUNNER_CREATE(argc, argv) \
-    (void)argc; (void)argv; \
-    _zf_test_runner _zf_test_runner_instance; \
-    _zf_test_runner_instance.nerror = 0;
+	(void)argc; (void)argv;
 
 #define TEST_SUIT_ARGUMENTS
 
 #define TEST_VERIFY_TRUE(a) \
-    if (!(a)) { \
-        fprintf(stderr, "%s:%u: %s:\n", __FILE__, __LINE__, "not true"); \
-        fprintf(stderr, "    false: %s\n", _ZF_TEST_STRINGIFY(a)); \
-    }
+	if (!(a)) { \
+		fprintf(stderr, "%s:%u: %s:\n", __FILE__, __LINE__, "not true"); \
+		fprintf(stderr, "    false: %s\n", _ZF_TEST_STRINGIFY(a)); \
+		exit(1); \
+	}
 
 #define TEST_VERIFY_FALSE(a) \
-    if ((a)) { \
-        fprintf(stderr, "%s:%u: %s:\n", __FILE__, __LINE__, "not false"); \
-        fprintf(stderr, "    true: %s\n", _ZF_TEST_STRINGIFY(a)); \
-    }
+	if ((a)) { \
+		fprintf(stderr, "%s:%u: %s:\n", __FILE__, __LINE__, "not false"); \
+		fprintf(stderr, "    true: %s\n", _ZF_TEST_STRINGIFY(a)); \
+		exit(1); \
+	}
 
 #define TEST_VERIFY_EQUAL(a, b) \
-    if (!((a) == (b))) { \
-        fprintf(stderr, "%s:%u: %s:\n", __FILE__, __LINE__, "not equal"); \
-        fprintf(stderr, "    left:   %s\n", _ZF_TEST_STRINGIFY(a)); \
-        fprintf(stderr, "    reight: %s\n", _ZF_TEST_STRINGIFY(b)); \
-    }
+	if (!((a) == (b))) { \
+		fprintf(stderr, "%s:%u: %s:\n", __FILE__, __LINE__, "not equal"); \
+		fprintf(stderr, "    left:   %s\n", _ZF_TEST_STRINGIFY(a)); \
+		fprintf(stderr, "    reight: %s\n", _ZF_TEST_STRINGIFY(b)); \
+		exit(1); \
+	}
 
 #define TEST_EXECUTE(f) \
-    f
+	f
 
 #define TEST_EXECUTE_SUITE(s) \
-    s()
+	s()
 
 #define TEST_RUNNER_EXIT_CODE() \
-    0
+	0
